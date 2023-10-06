55a119789effa1fb28a23af3802e13b671e785b1
Harsha0212 <harshavardhan9675@gmail.com>
2023-10-06 14:27:49 -0400

    VariablesDemo file added

---
+++ src/com/example/core/VariablesDemo.java
@@ -0,0 +1,31 @@
+package com.example.core;
+
+public class VariablesDemo {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 25, secondNumber = 50;
+
+        double result = firstNumber + secondNumber;
+
+        System.out.println("Result: " + result);
+
+        result = firstNumber - secondNumber;
+
+        System.out.println("Result: " + result);
+
+        result = firstNumber * secondNumber;
+
+        System.out.println("Result: " + result);
+
+        result = (double) firstNumber / secondNumber;
+
+        System.out.println("Result: " + result);
+
+
+
+
+
+
+    }
+}
