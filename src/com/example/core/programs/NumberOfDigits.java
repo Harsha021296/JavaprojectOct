748461edd9523dcf2832d8baa7ff229c081ad948
Harsha0212 <harshavardhan9675@gmail.com>
2023-10-11 09:19:49 -0400

    Digit Count program Added

--- src/com/example/core/programs/NumberOfDigits.java
+++ src/com/example/core/programs/NumberOfDigits.java
@@ -2,7 +2,14 @@ package com.example.core.programs;
 
 public class NumberOfDigits {
     public static void main(String[] args) {
-        int i = 4536;
+        int number = 4536, count = 0;
+
+        while(number != 0){
+
+            number = number / 10;
+            count++;
+        }
+        System.out.println("The number of digits in the given number are: "+count);
 
     }
 }
